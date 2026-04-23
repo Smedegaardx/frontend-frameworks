f0ecb5af5e7108e5ec36f68d7c39ed8c440d1a4b
Smedegaardx <anan0013@stud.kea.dk>
2026-04-23 19:17:38 +0200

    Enkelte produkter gjort klikbare fra productlist

--- src/app/components/BuyButton.jsx
+++ src/app/components/BuyButton.jsx
@@ -5,7 +5,7 @@ const BuyButton = ({ onAddToCart }) => {
     <div>
       <button
         onClick={onAddToCart}
-        className="bg-none border border-[#F5A623] text-[#F5A623] hover:bg-[#F5A623] hover:text-white py-2 px-4 rounded-3xl font-text mt-2"
+        className="bg-none border border-[#F5A623] text-[#F5A623] hover:bg-[#F5A623] hover:text-white py-2 px-4 rounded-3xl font-text mt-2 cursor-pointer"
       >
         Læg i kurv <FiPlus className="inline ml-1" />
       </button>
--- src/app/components/Product.jsx
+++ src/app/components/Product.jsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 import BuyButton from "./BuyButton";
 
-const Product = ({ title, price, stock, thumbnail, onAddToCart }) => {
+const Product = ({ title, price, stock, thumbnail, onAddToCart, id }) => {
   return (
     <div className="bg-white p-4 shadow-md rounded-2xl">
-      <div className="relative w-full h-48">
-        <Image
-          src={thumbnail}
-          alt={title}
-          fill
-          className="object-contain rounded-2xl"
-        />
-      </div>
-      <h2 className="font-hero">{title}</h2>
-      <p className="font-text">Price: ${price.toFixed(2)}</p>
-      {/* <p>Stock: {stock}</p> */}
+      <Link href={`/single/${id}`} className="cursor-pointer">
+        <div className="relative w-full h-48">
+          <Image
+            src={thumbnail}
+            alt={title}
+            fill
+            className="object-contain rounded-2xl"
+          />
+        </div>
+        <h2 className="font-hero">{title}</h2>
+        <p className="font-text">Price: ${price.toFixed(2)}</p>
+        {/* <p>Stock: {stock}</p> */}
+      </Link>
       <BuyButton onAddToCart={onAddToCart} />
     </div>
   );
